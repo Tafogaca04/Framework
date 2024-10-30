64d20f7c7e046c08a8789a24ae6d48ebea63002f
Tafogaca04 <162452935+Tafogaca04@users.noreply.github.com>
2024-10-30 16:41:27 -0300

    Add files via upload

---
+++ Atividade_01.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Atividade_01 {
+
+
+@GetMapping("/compras")
+public String compras() {
+	return "compras.html";
+}
+@GetMapping("/vendas")
+public String vendas() {
+	return "vendas.html";
+}
+@GetMapping("/estoque")
+public String estoque() {
+	return "estoque.html";
+}
+}
